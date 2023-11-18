0c3a99ef9d1396775fb28be5b0911299793b9132
Sho Haneishi <haneishi0715@gmail.com>
2023-11-18 13:24:14 +0900

    add a calculation

--- test.bash
+++ test.bash
@@ -11,8 +11,7 @@ res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng ${LINENO}
-[ "${out}" = 55 ] || ng ${LINENO}
+[ "${out}" = 15 55 ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
